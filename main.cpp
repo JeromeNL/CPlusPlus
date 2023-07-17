f4a49ec1cfdf49a62601d62d3c072dd83c752113
JeromeNL <jw.kwetters@student.avans.nl>
2023-07-17 23:16:10 +0200

    Simple calculator

--- main.cpp
+++ main.cpp
@@ -2,19 +2,22 @@
 
 
 int main() {
-    double x = 3.99;
-    double y = 4;
-    double z;
 
-    z = std::max(x, y);
-    z = pow(2, 4);
-    z = sqrt(9);
-    z = abs(-3);
-    z = round (x);
-    z = ceil(x);
-    z = floor(x);
+    double a;
+    double b;
+    double c;
 
-    std::cout << z;
+    std::cout << "Enter side A: ";
+    std:: cin >> a;
+
+    std::cout << "Enter side B: ";
+    std::cin >> b;
+
+    a = pow(a, 2);
+    b = pow(b, 2);
+    c = sqrt(a + b);
+
+    std::cout << "side C: " << c;
 
     return 0;
 }
