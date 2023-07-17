13006e47b22913387eedf6582eb1d101ff101732
JeromeNL <jw.kwetters@student.avans.nl>
2023-07-17 23:00:39 +0200

    arithmetic operators

--- main.cpp
+++ main.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
-#include <vector>
 
-//typedef std::vector<std::pair<std::string, int>> pairlist_t;
-//typedef std::string text;
-//typedef int number_t;
-using text_t = std::string;
-using number_t = int;
 
 int main() {
 
-    text_t firstName = "Jerome";
-    text_t lastName = "De Vries";
-    number_t age = 20;
+    int students = 6 - 5 + 4 * 3 / 2;
+    students++;
+    students--;
 
-    std::cout << firstName << " " << lastName << " has this age: " << age;
+    students*=2;
+    int remainder = students % 3;
 
+    std::cout << remainder;
     return 0;
 }
