798ab3df84f119374e5a2ef418408e299d78fdf5
JeromeNL <jw.kwetters@student.avans.nl>
2023-07-18 23:17:23 +0200

    Recursion

--- main.cpp
+++ main.cpp
@@ -1,40 +1,28 @@
 #include <iostream>
 
+void walk(int steps);
+
 int main() {
 
-    int *pNum = NULL;
-
-    pNum = new int;
-
-    *pNum = 21;
-
-    std::cout << "address: " << pNum;
-
-    delete pNum;
-
-    char *pGrades = NULL;
-    int size;
-
-    std::cout << "How many grades?";
-
-    std::cin >> size;
-
-    pGrades = new char[size];
-
-    for(int i = 0; i < size; i++){
-        std::cout << "Enter grade #" << i + 1 << ": ";
-        std::cin >> pGrades[i];
-    }
-
-    for(int i = 0; i < size; i++){
-        std::cout << pGrades[i] << " ";
-    }
-
-    delete[] pGrades;
-
+    walk(10);
 
     return 0;
 }
+
+// iterative
+//void walk (int steps){
+//    for(int i = 0; i < steps; i++){
+//        std::cout << "You take a step!\n";
+//    }
+//}
+
+// Recursive (more time && memory)
+//void walk (int steps){
+//    if(steps > 0){
+//        std::cout << "You take a step\n";
+//        walk(steps - 1);
+//    }
+//}
 
 
 
