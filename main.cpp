a242e74272d9ede6ed687a9bc0f153d84e2ad50f
JeromeNL <jw.kwetters@student.avans.nl>
2023-07-18 16:11:13 +0200

    foreach loop

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 
-int myNum = 2; // Should be avoided as much as possible
 
 
 int main() {
-    // same as Java
-   std::string cars[] = {"vw", "volvo", "suzuki"};
-   char grades[]  {'A', 'B', 'C'};
 
-   for(int i = 0; i < sizeof(cars) / sizeof(std::string); i++){
-       std::cout << cars[i] << '\n';
-   }
+    std::string students[] = {"spongebob", "patrick", "mr. krabs"};
+
+    for(std::string student : students){
+        std::cout << student << '\n';
+    }
 
     return 0;
 }
