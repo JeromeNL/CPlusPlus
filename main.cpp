f02fe35b348bb70bf5227a699a625d2753b680fe
JeromeNL <jw.kwetters@student.avans.nl>
2023-07-18 10:06:41 +0200

    While loop

--- main.cpp
+++ main.cpp
@@ -3,22 +3,11 @@
 
 int main() {
 
-  std::string name;
+    std::string name;
 
-  std::cout << "Enter your name";
-  std::cin >> name;
-
-    if(name.length() > 12){
-        std::cout << "What a long name!";
+    while(name.empty()){ // same as C#
+        std::cout << "This is nothing!";
     }
-
-    name.empty(); // returns true when empty
-    name.append("@gmail.com");
-    name.at(0);
-    name.insert(0, "@");
-    name.find(' ');
-    name.erase(3, 4);
-
 
     return 0;
 }
