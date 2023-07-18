ccdb5b8f6f160ca418c00dcb758933d9a68602ff
JeromeNL <jw.kwetters@student.avans.nl>
2023-07-18 18:29:19 +0200

    Sort array

--- main.cpp
+++ main.cpp
@@ -1,36 +1,33 @@
 #include <iostream>
 
-
-int searchArray(int array[], int size, int element);
+void sort(int array[], int size);
 
 int main() {
 
-    int numbers[] = {1, 2,3,4,5,6,7,8,9,10};
-    int size = sizeof(numbers) / sizeof(numbers[0]);
-    int index;
-    int myNum;
+    int array[] = {10, 1, 9, 2, 8, 3, 7, 4, 6, 5};
+    int size = sizeof(array)/sizeof(array[0]);
 
-    std::cout << "enter number to search for: " << '\n';
-    std::cin >> myNum;
+    sort(array, size);
 
-    index = searchArray(numbers, size, myNum);
-
-    if(index != -1){
-        std::cout << myNum << " is at index " << index << '\n';
-    } else {
-        std::cout << "Not in the array";
+    for(int element : array){
+        std::cout << element << " ";
     }
 
 
     return 0;
 }
 
-
-int searchArray(int array[], int size, int element){
-    for(int i = 0; i < size; i++){
-        if(array[i] == element){
-            return i;
+void sort(int array[], int size){
+    int temp;
+    for(int i = 0; i < size - 1; i++){
+        for(int j = 0; j < size - i - 1; j++){
+            if(array[j] > array[j + 1]){
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
         }
     }
-    return -1;
 }
+
+
