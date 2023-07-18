44b5fe671727953b1ec3c0e81b3df6c2c59f66c5
JeromeNL <jw.kwetters@student.avans.nl>
2023-07-18 09:46:21 +0200

    Logical operators

--- main.cpp
+++ main.cpp
@@ -3,9 +3,14 @@
 
 int main() {
 
-    int grade = 65;
+    int temp;
 
-    grade >= 60 ? std::cout << "Pass" : std::cout << "Fail";
+    std::cout << "Enter the temp";
+    std::cin >> temp;
+
+    if(temp > 18 && temp < 28){
+        std::cout << "Nice weather!";
+    }
 
     return 0;
 }
