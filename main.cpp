4ec2d49739402cf7c96d4eb75cf397b8c1aeb995
JeromeNL <jw.kwetters@student.avans.nl>
2023-07-18 16:06:44 +0200

    iterates over array

--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@ int myNum = 2; // Should be avoided as much as possible
 int main() {
     // same as Java
    std::string cars[] = {"vw", "volvo", "suzuki"};
+   char grades[]  {'A', 'B', 'C'};
 
-   std::cout << cars[1] << '\n';
-
-   std::cout << sizeof(cars);
+   for(int i = 0; i < sizeof(cars) / sizeof(std::string); i++){
+       std::cout << cars[i] << '\n';
+   }
 
     return 0;
 }
