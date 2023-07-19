318dfecbc0bf60b5dda9741cc843d0bfef19313b
JeromeNL <jw.kwetters@student.avans.nl>
2023-07-19 10:05:43 +0200

    Objects

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 
-enum Day {
-    Monday,
-    Tuesday,
-    Wednesday
+class Human{
+public:
+    std::string name;
+    std::string occupation;
+    int age;
+
+    void eat(){
+        std::cout << "This person is eating";
+    }
+
+    void drink(){
+        std::cout << "This person is drinking";
+    }
+
 };
 
 int main() {
+
+    Human human1;
 
 
     return 0;
