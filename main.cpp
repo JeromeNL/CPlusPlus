4f0af2acb65de482bdfb6b62dc6bd9bb1dc57635
JeromeNL <jw.kwetters@student.avans.nl>
2023-07-19 10:24:43 +0200

    Constructor overloading

--- main.cpp
+++ main.cpp
@@ -11,12 +11,20 @@ public:
         this->age = age;
         this->gpa = gpa;
     }
+
+    Student(std::string name){
+        this->name = name;
+        this->age = age;
+        this->gpa = gpa;
+    }
+
+
 };
 
 int main() {
 
     Student student1("Jerome", 20, 1.3);
-
+    Student student2("Kees");
     return 0;
 }
 
